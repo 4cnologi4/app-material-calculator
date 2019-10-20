b56ae060e198576adbff41fc28100ac96b46b8be
4cnologi4 <daniell9404@outlook.com>
2019-10-19 23:11:34 -0500

    Corrigiendo errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,9 @@ export class AppComponent {
   name = "Angular Material Calculator";
   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   operators = ["+", "0", "-", "*", "/", "%"];
+  fin = ["C", "=", "<-"];
   display = "";
+  ope = "";
   a = 0;
   b = 0;
   total = 0;
@@ -24,26 +26,59 @@ export class AppComponent {
 
   operation(operator: string) {
     switch (operator) {
+      case "0":
+        this.getValue("0");
+        break;
       case "+":
-        this.a += Number.parseFloat(this.display);
+        this.a = Number.parseFloat(this.display);
         this.clear();
+        this.ope = "+";
         break;
       case "-":
-        console.log(operator);
+        this.a = Number.parseFloat(this.display);
+        this.clear();
+        this.ope = "-";
         break;
       case "*":
-        console.log(operator);
+        this.a = Number.parseFloat(this.display);
+        this.clear();
+        this.ope = "*";
         break;
       case "/":
-        console.log(operator);
+        this.a = Number.parseFloat(this.display);
+        this.clear();
+        this.ope = "/";
         break;
       case "%":
-        console.log(operator);
+        this.a = Number.parseFloat(this.display);
+        this.clear();
+        this.ope = "%";
+        break;
+      case "C":
+        this.clear();
+        break;
+      case "<-":
+        this.display = this.display.slice(0, this.display.length - 1);
         break;
       case "=":
-        this.b = Number.parseFloat(this.display);
-        this.total = this.a + this.b;
-        this.display = ""+this.total; //""+ Cast number to string
+        if (this.ope === "+") {
+          this.b = Number.parseFloat(this.display);
+          this.total = this.a + this.b;
+          this.display = "" + this.total; //""+ Cast number to string
+          console.log(this.a, " - ", this.b);
+        } else if (this.ope === "-") {
+          this.total = this.a - this.b;
+          this.display = "" + this.total; //""+ Cast number to string
+        } else if (this.ope === "*") {
+          this.total = this.a * this.b;
+          this.display = "" + this.total; //""+ Cast number to string
+        } else if (this.ope === "/") {
+          this.total = this.a / this.b;
+          this.display = "" + this.total; //""+ Cast number to string
+        } else if (this.ope === "%") {
+          this.total = this.a % this.b;
+          this.display = "" + this.total; //""+ Cast number to string
+        }
     }
   }
 }
